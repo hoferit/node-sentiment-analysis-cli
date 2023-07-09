aa43e81fdbada9dedec32e3f1ba402e7a0dbaae2
Michael Hofer <hoferdev.mi@gmail.com>
2023-07-09 08:39:36 +0200

    refactoring code

--- index.js
+++ index.js
@@ -1,63 +1,57 @@
 import fs from 'node:fs';
 import readline from 'node:readline';
 
-const input = process.argv[2];
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-if (!input) {
-  rl.question('Please provide a text string or file path: ', (answer) => {
-    let text;
-    if (answer.endsWith('.txt')) {
-      // If the input ends with .txt, assume it's a file path and read its contents
-      text = fs.readFileSync(answer, 'utf-8');
-    } else {
-      // Otherwise, assume it's a word and use it directly
-      text = answer;
-    }
-    analyzeSentiment(text);
-    rl.close();
+const getTextFromPrompt = () =>
+  new Promise((resolve) => {
+    rl.question('Please provide a text string or file path: ', (answer) => {
+      resolve(
+        answer.endsWith('.txt') ? fs.readFileSync(answer, 'utf-8') : answer,
+      );
+    });
   });
-} else {
-  let text;
-  if (input.endsWith('.txt')) {
-    // If the input ends with .txt, assume it's a file path and read its contents
-    text = fs.readFileSync(input, 'utf-8');
-  } else {
-    // Otherwise, assume it's a word and use it directly
-    text = input;
-  }
-  analyzeSentiment(text);
-}
+
+const getTextFromArgs = (input) =>
+  input.endsWith('.txt') ? fs.readFileSync(input, 'utf-8') : input;
 
 async function analyzeSentiment(text) {
-  await fetch('http://text-processing.com/api/sentiment/', {
-    method: 'POST',
-    body: new URLSearchParams({
-      text,
-    }),
-  })
-    .then((response) => response.json())
-    .then((json) => {
-      const sentimentData = JSON.stringify(json);
-      const array = JSON.parse(sentimentData);
-
-      const negativePercent = Math.round(array.probability.neg * 100) + '%';
-      const neutralPercent = Math.round(array.probability.neutral * 100) + '%';
-      const positivePercent = Math.round(array.probability.pos * 100) + '%';
-
-      const mood =
-        positivePercent > negativePercent
-          ? 'positive'
-          : positivePercent < negativePercent
-          ? 'negative'
-          : 'neutral';
-
-      console.log(`The mood of your text is ${mood}.`);
-      console.log(`Positive: ${positivePercent}`);
-      console.log(`Neutral: ${neutralPercent}`);
-      console.log(`Negative: ${negativePercent}`);
+  try {
+    const response = await fetch('http://text-processing.com/api/sentiment/', {
+      method: 'POST',
+      body: new URLSearchParams({ text }),
     });
+
+    const json = await response.json();
+
+    const negativePercent = Math.round(json.probability.neg * 100) + '%';
+    const neutralPercent = Math.round(json.probability.neutral * 100) + '%';
+    const positivePercent = Math.round(json.probability.pos * 100) + '%';
+
+    const mood =
+      positivePercent > negativePercent
+        ? 'positive'
+        : positivePercent < negativePercent
+        ? 'negative'
+        : 'neutral';
+
+    console.log(`The mood of your text is ${mood}.`);
+    console.log(`Positive: ${positivePercent}`);
+    console.log(`Neutral: ${neutralPercent}`);
+    console.log(`Negative: ${negativePercent}`);
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
+
+async function main() {
+  const input = process.argv[2];
+  const text = input ? getTextFromArgs(input) : await getTextFromPrompt();
+  await analyzeSentiment(text);
+  rl.close();
+}
+
+main().catch(console.error);
